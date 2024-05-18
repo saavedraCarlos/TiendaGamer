818ddd4a293ff4b75e385672c242135f3d9a4488
saavedraCarlos <jeansaavedra6485@politecnicomayor.edu.co>
2024-05-18 13:56:53 -0500

    coneccion base de datos

--- accion.php
+++ accion.php
@@ -1,29 +1,30 @@
 <?php
 
-#Inicializacion de la sesion
 session_start();
 
-// Conectar con la base de datos
-$db = new mysqli('localhost', 'root', '', 'tiendaGamer');
+
+$db = new mysqli('localhost', 'root', '', 'tiendagamer');
 if ($db->connect_error) {
-	die('Error de conexión con la base de datos: '  . $db->connect_error);
+    die('Error de conexión con la base de datos: ' . $db->connect_error);
 }
-// Recibir datos del formulario
+
+
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-// Consultar la base de datos para verificar las credenciales
-$sql = "SELECT * FROM user
-	WHERE username = '$username' AND  password  = MD5('$password')";
+
+$sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
 $result = $db->query($sql);
 
 if ($result->num_rows > 0) {
-	// Inicio de sesión exitoso
-	$_SESSION['username'] = $username;
-	header('Location:bienvenido.php');
+  
+    $_SESSION['username'] = $username;
+    header('Location: bienvenido.php');
 } else {
-	// Inicio de sesión fallido
-	echo 'Usuario o contraseña incorrectos.';
+
+    $_SESSION['error_message'] = 'Usuario o contraseña incorrectos.';
+    header('Location: login.php');
 }
+
 $db->close();
 ?>
--- login.php
+++ login.php
@@ -9,32 +9,23 @@
 <body>
     <div class="container">
         <h2>Iniciar sesión</h2>
-        <form action="login.php" method="POST">
+        <form action="accion.php" method="POST">
             <div class="input-container">
                 <label for="nombre">Username:</label>
-                <input type="text" id="nombre" name="nombre">
+                <input type="text" id="username" name="username">
             </div>
             <div class="input-container">
                 <label for="contrasena">Contraseña:</label>
-                <input type="password" id="contrasena" name="contrasena">
+                <input type="password" id="contrasena" name="password">
             </div>
             <button type="submit" name="enviar">Enviar</button>
         </form>
         <?php
         session_start();
 
-        if(isset($_POST['enviar'])) {
-            
-            if(empty($_POST['nombre']) || empty($_POST['contrasena'])) {
-                echo "<p class='error-message'>Por favor ingrese un nombre y una contraseña válidos.</p>";
-            } else {
-                $nombre = $_POST['nombre'];
-                $contrasena = $_POST['contrasena'];
-
-                $_SESSION['nombre'] = $nombre;
-                header('Location: bienvenido.php');
-                exit;
-            }
+        if(isset($_SESSION['error_message'])) {
+            echo "<p class='error-message'>{$_SESSION['error_message']}</p>";
+            unset($_SESSION['error_message']);
         }
         ?>
     </div>
